b042888f882ec18bb007957beee8bead41ed99d3
Ernandez D. Medeiros <ernandez.d.medeiros@gmail.com>
2025-11-28 12:25:58 -0300

    Initial project structure

---
+++ script.js
@@ -0,0 +1,14 @@
+lucide.createIcons();
+
+function openLink(url) {
+  window.open(url, "_blank");
+}
+
+function sendEmail() {
+  const a = document.createElement("a");
+  a.href = "mailto:ernandez.d.medeiros@gmail.com?subject=Contato%20via%20site";
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
